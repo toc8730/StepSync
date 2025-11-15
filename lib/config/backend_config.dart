16f2d2e4806a66bd11512976528e9e0074b2f915
swirlingvortex <goktugberatbas@gmail.com>
2025-11-15 10:01:47 -0500

    changed url

--- lib/config/backend_config.dart
+++ lib/config/backend_config.dart
@@ -1,5 +1,5 @@
 class BackendConfig {
-  static const String _remoteDefault = 'http://ec2-3-128-90-65.us-east-2.compute.amazonaws.com';
+  static const String _remoteDefault = 'https://stepsync.xyz';
   static const String _localDefault = 'http://127.0.0.1:5000';
 
   static const bool _useRemote = bool.fromEnvironment('USE_REMOTE_BACKEND', defaultValue: true);
